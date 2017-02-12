4826539e9b94e4cf2c37a55d2877393a4c2d4b3e
alvaro <alvaro@localhost.localdomain>
2017-02-12 19:00:13 -0200

    criada aba contato

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { ContactsPage } from '../contacts/contacts';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  tab1Root: any = ContactsPage;
 
   constructor(public navCtrl: NavController) {
     
